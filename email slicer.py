a7626f5f339d1f41bb84bbc6fa7bd1d0dbc69bfc
arbazkhan678 <63591449+arbazkhan678@users.noreply.github.com>
2021-06-29 11:51:24 +0530

    Update email slicer.py

--- email slicer.py
+++ email slicer.py
@@ -1,6 +1,8 @@
 # Email slicer
 while True:
-    email = input(" what isn your email").strip()
+    email = input(" what isn your email || Do you want to continue again ? (y/n)").strip()
+    if email.lower() == 'n':
+        break
     user_name = email[:email.index('@')]
     domain_name = email[email.index('@')+1:]
     output = " Your name is '{}'  and Your domain is '{}'".format(user_name,domain_name)
